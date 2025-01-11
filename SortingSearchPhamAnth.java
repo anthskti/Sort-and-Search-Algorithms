cf1508ca129cde195648b03894aa0e59e0d613c1
anth <phamanthony47@gmail.com>
2025-01-11 12:06:32 -0500

    Template

---
+++ SortingSearchPhamAnth.java
@@ -0,0 +1,54 @@
+package sortingsearchphamanth;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger; 
+
+/*
+* @author anthony pham
+*/
+
+public class SortingSearchPhamAnth {
+
+    // @param args for command line argument
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+    private static ArrayList<String> selectionSort() {
+        return null;
+    } 
+
+    private static ArrayList<String> insertionSort() {
+        return null; 
+    }
+
+    private static ArrayList<String> bubbleSort() {
+        return null;
+    }
+
+    private static ArrayList<String> mergeSort() {
+        return null;
+    }
+
+    private static ArrayList<String> quickSort() {
+        return null;
+    }
+
+    private static ArrayList<String> binarySearch() {
+        return null; 
+    }
+
+
+
+}
+
+
